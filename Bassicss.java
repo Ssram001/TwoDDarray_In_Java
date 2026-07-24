a9b3699712d3f0ffde1311b6a19e5323d786d650
Sabyasachi <krram368@gmail.com>
2026-07-25 00:37:51 +0530

    bassic

---
+++ Bassicss.java
@@ -0,0 +1,52 @@
+package MultiDiMensionsArrays;
+
+import java.util.Scanner;
+
+public class Bassicss {
+    static void main(String[] args) {
+//        int[][] arr=new int[3][4];
+//        int[][] arr = {{6,0,2,7},{1,3,7,2},{9,9,4,5}};
+//        for(int j=0;j<3;j++){
+//            for(int k=0;k<4;k++){
+//                System.out.println(arr[j][k]+" ");
+//            }
+//            System.out.println();
+//        }
+
+//        System.out.println(arr.length);//it gives number of rows
+//        System.out.println(arr[0].length);//it give number of columns
+
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int m = sc.nextInt();
+//        int [][] arr = new int[n][m];
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<m;j++){
+//                arr[i][j] = sc.nextInt();
+//            }
+//        }
+//        System.out.println();
+//
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<m;j++){
+//                System.out.println(arr[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+
+
+
+//        for sum of 2D arrays
+//        int sum = 0;
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<m;j++){
+//               sum+=arr[i][j];
+//            }
+//        }
+//            System.out.println(sum);
+
+
+
+    }
+}
